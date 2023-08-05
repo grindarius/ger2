c9ecb5a29cbe29f4cfd9bb1f57d1cca342501c34
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-08-06 00:02:29 +0700

    new account table with birthdate

--- packages/api/migrations/V13__create_account_table.sql
+++ packages/api/migrations/V13__create_account_table.sql
@@ -4,6 +4,7 @@ create table account (
     email varchar(64) not null unique,
     password text not null,
     role user_role not null,
+    birthdate date not null,
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now(),
     primary key (id)
