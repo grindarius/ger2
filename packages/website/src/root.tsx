99f7fdf03b7de363988a50c125a15e0f38c026a2
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-10-16 22:17:29 +0700

    add theme provider

--- packages/website/src/root.tsx
+++ packages/website/src/root.tsx
@@ -6,6 +6,7 @@ import {
 } from '@builder.io/qwik-city'
 
 import { RouterHead } from './components/router-head/router-head'
+import { useThemeProvider } from './theme-provider'
 
 import './global.css'
 
@@ -17,12 +18,26 @@ export default component$(() => {
    * Don't remove the `<head>` and `<body>` elements.
    */
 
+  useThemeProvider()
+
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <script dangerouslySetInnerHTML={`
+          // set to dark or light depends on prefers-color-scheme
+          if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            localStorage.setItem('theme', 'ger2-dark')
+          }
+
+          if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+            localStorage.setItem('theme', 'ger2-light')
+          }
+
+          document.documentElement.setAttribute('data-theme', localStorage.getItem('theme') || 'ger2-dark')`}
+        />
       </head>
       <body lang="en">
         <RouterOutlet />
