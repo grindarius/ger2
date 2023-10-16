f8778798966a4a6721ea7e72eb6149b62bcb889e
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-10-16 22:21:06 +0700

    theme change button

--- packages/website/src/components/navbar/navbar.tsx
+++ packages/website/src/components/navbar/navbar.tsx
@@ -1,14 +1,24 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useContext } from '@builder.io/qwik'
 
 import { SignedInUser } from '~/components/signed-in-user/signed-in-user'
+import { ThemeContext } from '~/theme-provider'
+import { MaterialSymbolsDarkModeOutlineRounded } from '../icons/mateiral-symbols/dark-mode-outline-rounded'
+import { MaterialSymbolsWbSunny } from '../icons/mateiral-symbols/wb-sunny'
 
 export const Navbar = component$(() => {
+  const themeContext = useContext(ThemeContext)
+
   return (
-    <div class="navbar bg-base-200">
+    <div class="navbar bg-neutral">
       <div class="flex-1">
         <a href="/" class="text-xl normal-case btn btn-ghost">ger2</a>
       </div>
-      <SignedInUser />
+      <div class="flex-none gap-x-2">
+        <button type="button" class="btn btn-circle btn-ghost" onClick$={() => { themeContext.value = themeContext.value === '' ? 'ger2-dark' : themeContext.value === 'ger2-dark' ? 'ger2-light' : 'ger2-dark' } }>
+          { themeContext.value === '' || themeContext.value === 'ger2-dark' ? <MaterialSymbolsDarkModeOutlineRounded class="w-6 h-5" /> : <MaterialSymbolsWbSunny class="w-6 h-6" /> }
+        </button>
+        <SignedInUser />
+      </div>
     </div>
   )
 })
