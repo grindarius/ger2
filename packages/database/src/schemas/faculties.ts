5970a264fb1c24cab0b26e59eea66436627cbee2
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-07-28 14:09:15 +0700

    new explanation to faculties

--- packages/database/src/schemas/faculties.ts
+++ packages/database/src/schemas/faculties.ts
@@ -1,10 +1,10 @@
+import { relations } from 'drizzle-orm'
 import { pgTable, varchar } from 'drizzle-orm/pg-core'
 import { TIMESTAMP_COLUMNS } from '../utils.js'
-import { relations } from 'drizzle-orm'
 import { majors } from './majors.js'
 
 /**
- * Stores information about a faculty inside a university. 
+ * Stores information about a faculty inside a university.
  * e.g. `Faculty of Science` or `Faculty of Social Science`.
  */
 export const faculties = pgTable('faculties', {
