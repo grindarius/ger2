fe8572dccef736ee22df803b7495502a8a06e80a
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-07-28 12:44:35 +0700

    rooms and buildings schema

---
+++ packages/database/src/schemas/rooms.ts
@@ -0,0 +1,20 @@
+import { relations } from 'drizzle-orm'
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
+import { TIMESTAMP_COLUMNS } from '../utils.js'
+import { buildings } from './buildings.js'
+
+export const rooms = pgTable('rooms', {
+  id: varchar('id', { length: 26 }).notNull().primaryKey(),
+  buildingId: varchar('building_id', { length: 26 }).notNull(),
+  name: varchar('name', { length: 512 }).notNull(),
+  capacity: integer('capacity').notNull(),
+  floor: integer('floor').notNull().default(1),
+  ...TIMESTAMP_COLUMNS
+})
+
+export const roomsRelations = relations(rooms, ({ one }) => ({
+  building: one(buildings, {
+    fields: [rooms.buildingId],
+    references: [buildings.id]
+  })
+}))
