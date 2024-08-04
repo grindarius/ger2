d62a5013ee512e7bbd04457ad6f8e42ddb9bf23f
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-08-04 19:29:04 +0700

    move to dedicated seed module

--- packages/scripts/src/seed/save-json-file.ts
+++
@@ -1,8 +0,0 @@
-import { writeFile } from 'node:fs/promises'
-import chalk from 'chalk'
-
-export const toJson = async <T extends object>(value: T, filename: string): Promise<void> => {
-  console.log(chalk.yellow(`- Saving ./src/json/${filename}.json`))
-  await writeFile(`./src/json/${filename}.json`, JSON.stringify(value, null, 2), 'utf-8')
-  console.log(chalk.greenBright(`- Successfully saved ./src/json/${filename}.json`))
-}
