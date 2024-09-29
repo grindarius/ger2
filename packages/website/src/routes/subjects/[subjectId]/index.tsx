1ae7f7af82019418299c727d94958cb507f2a6a8
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-09-29 13:01:55 +0700

    more description

--- packages/website/src/routes/subjects/[subjectId]/index.tsx
+++ packages/website/src/routes/subjects/[subjectId]/index.tsx
@@ -4,6 +4,8 @@ export default component$(() => {
   return (
     <div>
       Show information about a subject based on given semester id and stuffs through query params
+
+      Have a tab to check history of the subject. which year it gets put on and stuffs.
     </div>
   )
 })
--- packages/website/src/routes/subjects/search/index.tsx
+++ packages/website/src/routes/subjects/search/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik'
 
 export default component$(() => {
-  return <div>A page for searching an opening subjects for each semester.</div>
+  return <div>A page for searching an opening subjects for each semester. Have options for searching subjects and redirects to the subject information page.</div>
 })
